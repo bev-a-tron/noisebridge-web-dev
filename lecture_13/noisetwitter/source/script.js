e43c0b4c284772cad134650a1db7f84aa0485ab0
Beverly Lau <beverly.a.lau@gmail.com>
2015-10-26 21:54:52 -0700

    add route provider

--- lecture_13/noisetwitter/source/script.js
+++ lecture_13/noisetwitter/source/script.js
@@ -1,28 +1,55 @@
 var tweets = $('.tweets');
 
 
-angular.module('NoiseTwitter', []);
+angular.module('NoiseTwitter', ['ngRoute']);
+
+angular.module('NoiseTwitter').config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/', {
+    controller: 'MainController',
+    templateUrl: 'main.html'  // ng-view says to replace its element with the template that has id='main.html'
+  }).when('/tweets/:id', {
+    controller: 'ShowController',
+    templateUrl: 'show.html',
+    /* resolve: {
+      tweets:
+        $.ajax('data/tweets.json')
+      } */
+  });
+}]);
+
+
+
+angular.module('NoiseTwitter').run(['$rootScope', function ($rootScope) {
+  //$.ajax('data/tweets.json').done(function (response) {
+  //  $rootScope.$apply(function () {
+  //    $rootScope.tweets = response;
+  //  })
+  //})
+
+  $rootScope.tweets = [
+    {
+      "id": 2,
+      "author": "JeffreyATW",
+      "time": "1h",
+      "content": "You give love a bad name."
+    },
+    {
+      "id": 1,
+      "author": "JeffreyATW",
+      "time": "2h",
+      "content": "Old McDonald had a farm"
+    },
+    {
+      "id": 0,
+      "author": "JeffreyATW",
+      "time": "3h",
+      "content": "I'm a little teapot, short and stout."
+    }
+  ]
+
+}]);
 
 angular.module('NoiseTwitter').controller('MainController', ['$scope', function($scope) {
-  $scope.hello = "Hi, there!";
-
-  $scope.tweets = [
-    {
-      author: 'JeffreyATW',
-      time: '1h',
-      content: 'You give love a bad name.',
-    },
-    {
-      author: 'JeffreyATW',
-      time: '2h',
-      content: 'Old McDonald had a farm',
-    },
-    {
-      author: 'JeffreyATW',
-      time: '3h',
-      content: 'I\'m a little teapot, short and stout.',
-    }
-  ];
 
   $scope.addNewTweet = function () {
     var newTweetObj = {
@@ -39,6 +66,14 @@ angular.module('NoiseTwitter').controller('MainController', ['$scope', function(
 
 // each controller has its own scope
 // scopes cascade, overwriting higher nested scopes
+
+angular.module('NoiseTwitter').controller('ShowController', ['$routeParams', '$scope', function ($routeParams, $scope) {
+  for (var i = 0; i < $scope.tweets.length; i += 1) {
+    if ($routeParams.id == $scope.tweets[i].id) {
+      $scope.tweet = $scope.tweets[i]
+    }
+  }
+}]);
 
 angular.module('NoiseTwitter').directive('tweet', [function () {
   return {
